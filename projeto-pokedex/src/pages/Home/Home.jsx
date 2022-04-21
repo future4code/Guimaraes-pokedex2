3343bd7ff9a0773ad1463aa87fa395ae3623c84d
Renan Arimatea <renanmoraiz@gmail.com>
2022-04-21 10:26:26 -0300

    Realizado a estilização da home.

--- projeto-pokedex/src/pages/Home/Home.jsx
+++ projeto-pokedex/src/pages/Home/Home.jsx
@@ -1,28 +1,38 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import { goToPokedex, goToPokeDetail } from "../../Navigator/navigation";
-import { HomeBox } from "./HomeStyled";
+import { BtnStyled, HomeBox, HomePageBg, InfoBox, VideoBg } from "./HomeStyled";
+import sample from "../../Assets/video/Pokevideo4.mp4"
+
+
 const Home = () => {
   const navigate = useNavigate();
   return (
     <HomeBox>
-      <h3>Boas vindas à Pokedex!</h3>
-      <h5>Qual a boa de hoje?</h5>
 
-      <button
+      <HomePageBg>
+        <VideoBg autoPlay loop muted src={sample} type='video/mp4'/>
+      </HomePageBg>
+
+      <InfoBox>
+        <h3>Boas vindas à <span>Pokedex</span></h3>
+        {/* <h5>Qual a boa de hoje?</h5> */}
+      </InfoBox>
+
+      <BtnStyled
         onClick={() => {
           goToPokedex(navigate);
         }}
       >
         Ir para Pokedex
-      </button>
-      <button
+      </BtnStyled>
+      <BtnStyled
         onClick={() => {
           goToPokeDetail(navigate);
         }}
       >
         Destaques dos Pokémons
-      </button>
+      </BtnStyled>
     </HomeBox>
   );
 };
--- projeto-pokedex/src/pages/Home/HomeStyled.js
+++ projeto-pokedex/src/pages/Home/HomeStyled.js
@@ -2,8 +2,125 @@ import styled from "styled-components";
 
 export const HomeBox = styled.div`
 
-    height: 100vh;
+    min-height: 100vh;
     width: auto;
 
+    text-align: center;
+`
+
+export const HomePageBg = styled.div`
+
+    position: absolute;
+    top: 0;
+`
+
+export const VideoBg = styled.video`
+
+    position: fixed;
+    width: 110%;
+    height: 100%;
+    object-fit: cover;
+    z-index: -1;
+`
+
+export const InfoBox = styled.div`
+
+    max-height: 35em;
+    width: 40vw;
+    border: 1px solid yellow;
+    border-radius: 1em;
+    margin: 3em auto;
+    padding: 0 1em 1em 1em;
+    background-color: rgba(0, 0, 0, 0.8);
+
+    h3 {
+        font-family: "Pokefont";
+        color: #FFF;
+        font-size: 2.5em;
+    }
+
+    span {
+        font-family: "Pokefont";
+        color: yellow;
+
+        -webkit-animation-name: blinker;
+        -webkit-animation-duration: 1s;
+        -webkit-animation-timing-function: linear;
+        -webkit-animation-iteration-count: infinite;
+        -moz-animation-name: blinker;
+        -moz-animation-duration: 1s;
+        -moz-animation-timing-function: linear;
+        -moz-animation-iteration-count: infinite;
+        animation-name: blinker;
+        animation-duration: 1s;
+        animation-timing-function: linear;
+        animation-iteration-count: infinite;
+
+        @-moz-keyframes blinker {
+            0% {
+                opacity: 1.0;
+            }
+            50% {
+                opacity: 0.0;
+            }
+            100% {
+                opacity: 1.0;
+            }
+        }
+        
+        @-webkit-keyframes blinker {
+            0% {
+                opacity: 1.0;
+            }
+            50% {
+                opacity: 0.0;
+            }
+            100% {
+                opacity: 1.0;
+            }
+        }
+
+        @keyframes blinker {
+            0% {
+                opacity: 1.0;
+            }
+            50% {
+                opacity: 0.0;
+            }
+            100% {
+                opacity: 1.0;
+            }
+        }
+    }
+
+    h5 {
+        color: #FFF;
+        font-size: 1.8em;
+    }
+
+`
+
+export const BtnStyled = styled.button`
+
+
+    font-family: "Pokefont";
+    font-size: 1em;
+
+    color: #FFF;
+    background-color: rgba(0, 0, 0, 0.8);
+
     padding: 1em;
+    margin: 1em;
+    width: 15em;
+
+    border: 1px solid #FFF;
+    border-radius: 1em;
+
+    :hover {
+        color: yellow;
+        border-color: yellow;
+        transform: scale(1.1);
+        transition: .3s;
+        cursor: pointer;
+    }
 `
